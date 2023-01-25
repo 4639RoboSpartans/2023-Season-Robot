3b5c659d99b91b92f747bf8f7864e561f7e33874
JonnyJonnyBoy <n.jonathan.jiang@gmail.com>
2023-01-24 20:23:48 -0600

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,15 +4,12 @@
 
 package frc.robot;
 
-<<<<<<< HEAD
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-=======
->>>>>>> 02929973d41ea6c7b8aed37cabf6a67b47c933ab
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -39,20 +36,10 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-<<<<<<< HEAD
-    m_robotContainer = new RobotContainer();
-
-    tableInstance = NetworkTableInstance.getDefault();
-    table = tableInstance.getTable("limelight");
-
-    table.getEntry("camMode").setNumber(0);
-    table.getEntry("pipeline").setNumber(0);
-=======
     robotContainer = new RobotContainer();
 
     // Start Vision thread
     // aprilTagReader = new AprilTagReader(640, 480);
->>>>>>> 02929973d41ea6c7b8aed37cabf6a67b47c933ab
   }
 
   NetworkTableInstance tableInstance;
