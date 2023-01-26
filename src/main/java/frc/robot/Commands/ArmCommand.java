24d9624e4a829506814446094d7a350e1ad0e973
NJonathanJiang <n.jonathan.jiang@gmail.co,m>
2023-01-25 19:20:33 -0600

    yep

--- src/main/java/frc/robot/Commands/ArmCommand.java
+++ src/main/java/frc/robot/Commands/ArmCommand.java
@@ -2,8 +2,8 @@ package frc.robot.Commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
-import frc.robot.OI;
 import frc.robot.Subsystem.ArmPivotSubsystem;
+import frc.robot.Util.OI;
 
 public class ArmCommand extends CommandBase {
     private final OI oi;
--- src/main/java/frc/robot/Vision/LimeLight.java
+++ src/main/java/frc/robot/Vision/LimeLight.java
@@ -1,45 +1,13 @@
 package frc.robot.Vision;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Util.Network.Network;
+import frc.robot.Util.Network.Table;
 
 public class LimeLight {
-    private static LimeLight instance = null;
-    public static LimeLight getInstance(){
-        return instance == null ? instance = new LimeLight() : instance;
-    }
-
-
-    private NetworkTableInstance tableInstance;
-    private NetworkTable limelightTable;
-    private Map<String, NetworkTableEntry> entryCache = new HashMap<>();
-    private LimeLight(){
-        tableInstance = NetworkTableInstance.getDefault();
-        limelightTable = tableInstance.getTable("limelight");
-
-    }
-
-    private NetworkTableEntry getEntry(String key){
-        if(entryCache.containsKey(key))
-            return entryCache.get(key);
-        var entry = limelightTable.getEntry(key);
-        entryCache.put(key, entry);
-        return entry;
-    }
-
-    public double getNumber(String key){
-        return (double) getEntry(key).getNumber(0.0);
-    }
-
-    public int getInteger(String key){
-        return (int) getEntry(key).getInteger(0);
-    }
-
-    public String getString(String key){
-        return getEntry(key).getString("");
+    private static Table table;
+    public static Table getTable() {
+        if(table == null)
+            table = Network.getInstance().getTable("limelight");
+        return table;
     }
 }
