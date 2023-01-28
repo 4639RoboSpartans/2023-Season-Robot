959eb02b90eb1c13473074fc21410f428932a54e
Santiago Espinoza <89491348+Santiago11234@users.noreply.github.com>
2023-01-28 16:30:53 -0600

    Add files via upload

---
+++ PneumaticSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class PneumaticSubsystem extends SubsystemBase{
+    private final Solenoid m_solenoid;
+
+    public PneumaticSubsystem(){
+        m_solenoid = new Solenoid(PneumaticsModuleType.CTREPCM, 1);
+    } 
+    public void extendPiston() {
+        m_solenoid.set(true);
+    }
+    public void RetractPiston() {
+        m_solenoid.set(false);
+    }
+}
