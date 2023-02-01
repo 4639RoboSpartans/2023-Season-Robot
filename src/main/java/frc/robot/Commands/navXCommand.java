2ef9a660c6f56952b36376ce70f0fdfd53da68ad
JonnyJonnyBoy <n.jonathan.jiang@gmail.com>
2023-01-31 19:51:31 -0600

    navXCommand

---
+++ src/main/java/frc/robot/Commands/navXCommand.java
@@ -0,0 +1,56 @@
+package frc.robot.Commands;
+//if angle is not flat go "uphill"
+//if angle is flat for 3 seconds, isFinisehd returns true
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
+//to move forward/backwards do: swerve.setAllState(new SwerveModuleState(speed, new Rotation2d()))
+//getPitch() - angle of the front of the robot off the ground 
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Subsystem.SwerveDriveSubsystem;
+import frc.robot.Subsystem.navXSubsystem;
+
+public class navXCommand extends CommandBase{
+    private final SwerveDriveSubsystem swerve;
+    private final navXSubsystem navx;
+    private double flatTime;
+    private double lastFrameTime;
+
+    public navXCommand(SwerveDriveSubsystem swerve, navXSubsystem navx){
+        this.swerve = swerve;
+        this.navx = navx;
+        addRequirements(swerve);
+        flatTime = 0.0;
+        lastFrameTime = Timer.getFPGATimestamp();
+        
+
+    }
+    public void initialize(){
+
+    }
+    public void execute(){
+
+        if(!navx.isZero()){
+            double speed = Math.max(Math.min(navx.readPitch()/11, 1), -1);
+            swerve.setAllModules(new SwerveModuleState(speed, new Rotation2d()));
+            flatTime = 0;
+        }   
+        else{
+
+            flatTime += Timer.getFPGATimestamp()- lastFrameTime;
+            lastFrameTime = Timer.getFPGATimestamp();
+
+        }
+
+     }
+    public void end(){
+
+    }
+    public boolean isFinished(){
+
+        return (flatTime<=3);
+
+    }
+    
+}
