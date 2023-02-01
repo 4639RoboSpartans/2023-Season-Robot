c53f2a742a54a1c4f773e4dc575d510f08106a20
JonnyJonnyBoy <n.jonathan.jiang@gmail.com>
2023-01-31 19:08:23 -0600

    navX

---
+++ src/main/java/frc/robot/Subsystem/navXSubsystem.java
@@ -0,0 +1,27 @@
+package frc.robot.Subsystem;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class navXSubsystem extends SubsystemBase{
+    private final AHRS ahrs;
+    private final PIDController pid;
+    public navXSubsystem(){
+        ahrs = new AHRS();
+        pid = new PIDController(0, 0, 0);
+    }
+
+    public float readPitch(){
+        return ahrs.getPitch();
+    }
+
+    public boolean isZero(){
+        float pitch = ahrs.getPitch();
+        if(Math.abs(pitch)<1.0){
+            return true;
+        }
+        return false;
+    }
+
+    
+
+    }
