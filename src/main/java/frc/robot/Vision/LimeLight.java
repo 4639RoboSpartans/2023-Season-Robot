b31b7520d41cfa455a1fa6338746f77058966886
NJonathanJiang <n.jonathan.jiang@gmail.co,m>
2023-02-02 08:43:21 -0600

    .

--- src/main/java/frc/robot/Vision/LimeLight.java
+++ src/main/java/frc/robot/Vision/LimeLight.java
@@ -4,10 +4,8 @@ import frc.robot.Util.Network.Network;
 import frc.robot.Util.Network.Table;
 
 public class LimeLight {
-    private static Table table;
-    public static Table getTable() {
-        if(table == null)
-            table = Network.getInstance().getTable("limelight");
-        return table;
+    public void getOffsetFromCenteredAprilTag(){
+        var table = Network.getTable("campose");
+        // TODO: finish implementation
     }
 }
