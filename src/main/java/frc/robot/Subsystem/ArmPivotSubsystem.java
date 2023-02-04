32dab2ce4cc9b6a10a07e2d1d47f78f9953a1c31
Santiago11234 <santiagogeb@gmail.com>
2023-02-03 21:36:17 -0600

    limelight is bad, vision code was added, arm code

--- src/main/java/frc/robot/Subsystem/ArmPivotSubsystem.java
+++ src/main/java/frc/robot/Subsystem/ArmPivotSubsystem.java
@@ -9,33 +9,45 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmPivotSubsystem extends SubsystemBase {
-    private final WPI_VictorSPX armPivotMotor;
+    private final WPI_VictorSPX armPivotMotor1;
+    private final WPI_VictorSPX armPivotMotor2;
+
     private final PIDController pid; 
     private final CANCoder canCoder;
 
     public ArmPivotSubsystem(){
+
         pid = new PIDController(0, 0, 0);
-        armPivotMotor = new WPI_VictorSPX(Constants.IDs.ARM_PIVOT_ID);
-        armPivotMotor.configFactoryDefault();
-        armPivotMotor.setNeutralMode(NeutralMode.Brake);
+
+        armPivotMotor1 = new WPI_VictorSPX(Constants.IDs.ARM_PIVOT1_ID);
+        armPivotMotor1.configFactoryDefault();
+        armPivotMotor1.setNeutralMode(NeutralMode.Brake);
+
+        armPivotMotor2 = new WPI_VictorSPX(Constants.IDs.ARM_PIVOT2_ID);
+        armPivotMotor2.configFactoryDefault();
+        armPivotMotor2.setNeutralMode(NeutralMode.Brake);
+
         canCoder = new CANCoder(0);
     }
 
     public void stop(){
-        armPivotMotor.stopMotor();
+        armPivotMotor1.stopMotor();
+        armPivotMotor2.stopMotor();
     }
 
     public void set(double speed){
-        armPivotMotor.set(speed);
+        armPivotMotor1.set(speed);
+        armPivotMotor2.set(-speed);
     }
 
     public void setAngle(double angle){
+        pid.setSetpoint(angle);
         pid.setSetpoint(angle);
     }
 
 
     @Override
-    public void periodic() {
+    public void periodic( ) {
        double speed =  pid.calculate(canCoder.getAbsolutePosition());
        set(speed);
     }
