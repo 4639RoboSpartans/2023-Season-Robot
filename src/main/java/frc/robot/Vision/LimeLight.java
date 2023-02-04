b0fcac87ababe3d11ee642756190a9f1fb88b589
Santiago11234 <santiagogeb@gmail.com>
2023-02-04 13:43:27 -0600

    l

--- src/main/java/frc/robot/Vision/LimeLight.java
+++ src/main/java/frc/robot/Vision/LimeLight.java
@@ -1,6 +1,7 @@
 package frc.robot.Vision;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Util.Network.Network;
 import frc.robot.Util.Network.Table;
 
@@ -8,6 +9,11 @@ public class LimeLight {
     public void getOffsetFromCenteredAprilTag(){
         var table = Network.getTable("campose");
         // TODO: finish implementation
+        SmartDashboard.putNumber("tv", table.getEntry("tv").getDouble(0));
+        SmartDashboard.putNumber("tx", table.getEntry("tx").getDouble(0));
+        SmartDashboard.putNumber("ty", table.getEntry("ty").getDouble(0));
+        SmartDashboard.putNumber("ta", table.getEntry("ta").getDouble(0));
+        
     }
 
      
